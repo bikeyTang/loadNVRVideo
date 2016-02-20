e4d2b51b9ca0866386e33ec680bcbaa42c7268ce
bikeyTang <tjpu_tangkun@126.com>
2016-02-20 17:32:33 +0800

    upate split

--- vedioloader/videoloader.cpp
+++ vedioloader/videoloader.cpp
@@ -8,7 +8,9 @@
 #include <string>
 #include <time.h>
 
-#define CCT (+8)
+using std::cout;
+using std::endl;
+using std::string;
 const int splitMinuteTime = 10;
 struct ConnectInfo{
 	char ip[16];
@@ -27,11 +29,7 @@ int getVedioByTime(int UserID, LONG chanel, NET_DVR_TIME *startTime, NET_DVR_TIM
 			endTime->dwYear, endTime->dwMonth, endTime->dwDay, endTime->dwHour, endTime->dwMinute, endTime->dwSecond);
 		path = storeFileName;
 	}
-	std::string s_path(path);
-	std::size_t l_position=s_path.find_last_of("/");
-	std::string dir = s_path.substr(0, l_position);
-	
-	std::cout <<"path:"<< path << std::endl;
+	cout <<"path:"<< path << endl;
 	NET_DVR_PLAYCOND struFileCond = { 0 };
 	//通道号
 	struFileCond.dwChannel = chanel;
@@ -41,43 +39,86 @@ int getVedioByTime(int UserID, LONG chanel, NET_DVR_TIME *startTime, NET_DVR_TIM
 	struFileCond.struStopTime = *endTime;
 	int IFindHandle = NET_DVR_GetFileByTime_V40(UserID, path, &struFileCond);
 	if (IFindHandle < 0){
-		std::cout << "error:find file fail " << NET_DVR_GetLastError() << std::endl;
+		cout << "error:create file fail " << NET_DVR_GetLastError() << endl;
 		return -1;
 	}
 	if (!NET_DVR_PlayBackControl_V40(IFindHandle, NET_DVR_PLAYSTART, NULL, 0, NULL, NULL)){
-		std::cout << "error:play back control failed " << NET_DVR_GetLastError() << std::endl;
-		NET_DVR_Logout(UserID);
-		NET_DVR_Cleanup();
+		cout << "error:play back control failed " << NET_DVR_GetLastError() <<endl;
 		return -1;
 	}
 	//视频下载
 	int nPos = 0;
 	for (nPos = 0; nPos < 100 && nPos >= 0; nPos = NET_DVR_GetDownloadPos(IFindHandle)){
-		std::cout <<"progress:"<< nPos << std::endl;
+		//cout <<"progress:"<< nPos << endl;
 		Sleep(5000);
 	}
 	if (nPos<0 || nPos>100){
-		std::cout << "error:downloading problem " << NET_DVR_GetLastError() << std::endl;
+		cout << "error:downloading problem " << NET_DVR_GetLastError() <<endl;
 		NET_DVR_Logout(UserID);
 		NET_DVR_Cleanup();
 		return -1;
 	}
 	if (!NET_DVR_StopPlayBack(IFindHandle)){
-		std::cout << "error:stop failed " << NET_DVR_GetLastError() << std::endl;
+		cout << "error:stop failed " << NET_DVR_GetLastError() <<endl;
 		return -1;
 	}
-	std::cout << "progress:" << nPos << std::endl;
-	return 0;
+	//cout << "progress:" << nPos << endl;
+	return nPos;
+}
+//查找给定时间段内的文件数
+int videoFileCount(LONG IUserID,LONG chanel,NET_DVR_TIME *startTime,NET_DVR_TIME *endTime){
+	int fileCount=0;
+	NET_DVR_FILECOND_V40 struFileCond = { 0 };
+	struFileCond.dwFileType = 0xFF;
+	struFileCond.lChannel = chanel;
+	struFileCond.dwUseCardNo = 0;
+	struFileCond.struStartTime = *startTime;
+	struFileCond.struStopTime = *endTime;
+
+	LONG IFindHandle = NET_DVR_FindFile_V40(IUserID, &struFileCond);
+	if (IFindHandle < 0){
+		cout << "error:" << NET_DVR_GetLastError() << endl;
+		return -1;
+	}
+	NET_DVR_FINDDATA_V40 struFileData;
+	while (true)
+	{
+		//逐个获取查找到的文件信息
+		int result = NET_DVR_FindNextFile_V40(IFindHandle, &struFileData);
+		if (result == NET_DVR_ISFINDING)
+		{
+			continue;
+		}
+		else if (result == NET_DVR_FILE_SUCCESS) //获取文件信息成功
+		{
+			++fileCount;
+			continue;
+		}
+		else if (result == NET_DVR_FILE_NOFIND || result == NET_DVR_NOMOREFILE) //未查找到文件或者查找结束
+		{
+			break;
+		}
+		else
+		{
+			cout << "error:find file fail for illegal get file state" << endl;
+			break;
+		}
+	}
+	if (IFindHandle>=0)
+	{
+		NET_DVR_FindClose_V30(IFindHandle);
+	}
+	return fileCount;
 }
 /*
 把时间格式转化成NET_DVR_TIME
 */
 void parseTime(char *t, NET_DVR_TIME &ndt){
-	std::string dateTime = t;
+	string dateTime = t;
 	std::size_t y, mon, d, h, m;
 	y = dateTime.find_first_of("-");
 	ndt.dwYear = atoi(dateTime.substr(0, y).c_str());
-	std::string tempStr = dateTime.substr(y + 1);
+	string tempStr = dateTime.substr(y + 1);
 	mon = tempStr.find_first_of("-");
 	ndt.dwMonth = atoi(tempStr.substr(0, mon).c_str());
 	tempStr = tempStr.substr(mon + 1);
@@ -98,7 +139,7 @@ void parseTime(char *t, NET_DVR_TIME &ndt){
 ConnectInfo parseParam(int argc, char *arg[]){
 	ConnectInfo ci;
 	for (int i = 1; i < argc; i += 2){
-		std::string param(arg[i]);
+		string param(arg[i]);
 		char* value = arg[i + 1];
 
 		if (param == "-ip"){
@@ -179,7 +220,7 @@ int main(int argc, char* argv[])
 	NET_DVR_DEVICEINFO_V30 struDeviceInfo;
 	IUserID = NET_DVR_Login_V30(ci.ip, ci.port, ci.userName, ci.password, &struDeviceInfo);
 	if (IUserID < 0){
-		std::cout << "error:Login failed " << NET_DVR_GetLastError() << std::endl;
+		cout << "error:Login failed " << NET_DVR_GetLastError() <<endl;
 		NET_DVR_Cleanup();
 		return -1;
 	}
@@ -187,7 +228,7 @@ int main(int argc, char* argv[])
 	//判断输入的通道号是否在范围内
 	int chanelNum = (int)struDeviceInfo.byIPChanNum;
 	if (ci.chanel>chanelNum){
-		std::cout << "error:chanel out of arrange" << std::endl;
+		cout << "error:chanel out of arrange" << endl;
 		return -1;
 	}
 	int inputChanel = startChanel + ci.chanel - 1;
@@ -196,11 +237,49 @@ int main(int argc, char* argv[])
 
 	NET_DVR_TIME splitEndTime,splitStartTime;
 	splitStartTime = ci.startTime;
-	
+	int fc = 0,fn=0;
+	string path;
+	if (ci.path != NULL){
+		string vPath(ci.path);
+		string::size_type pos = vPath.find_last_of(".");
+		path = vPath.substr(0, pos);
+	}
 	for (int i = 0; i < splitNum; i++){
 		
 		splitEndTime = getTimeSum(&splitStartTime, splitMinuteTime * 60);
-		getVedioByTime(IUserID, inputChanel, &splitStartTime, &splitEndTime, ci.path);
+		//查找给定时间段内是否存在文件
+		fc = videoFileCount(IUserID, inputChanel, &splitStartTime, &splitEndTime);
+		//不存在则跳过
+		if (fc <= 0){
+			continue;
+		}
+		++fn;
+		int p = 0;
+		
+		if (ci.path == NULL){
+			p = getVedioByTime(IUserID, inputChanel, &splitStartTime, &splitEndTime, nullptr);
+		}
+		else{
+			
+			string storePath = path + "_";
+			char fnChar[5];
+			sprintf_s(fnChar, "%d", fn);
+			storePath += fnChar;
+			storePath += ".mp4";
+			p = getVedioByTime(IUserID, inputChanel, &splitStartTime, &splitEndTime, const_cast<char*>(storePath.c_str()));
+		}
+		
+		if (p == 100){
+			if (remTime > 0){
+				cout << "progress:" << (i + 1) * 100 / (splitNum + 1) << endl;
+			}
+			else{
+				cout << "progress:" << (i + 1) * 100 / splitNum << endl;
+			}
+		}
+		else {
+			return -1;
+		}
 		splitStartTime.dwYear=splitEndTime.dwYear;
 		splitStartTime.dwMonth = splitEndTime.dwMonth;
 		splitStartTime.dwDay = splitEndTime.dwDay;
@@ -209,7 +288,27 @@ int main(int argc, char* argv[])
 		splitStartTime.dwSecond = splitEndTime.dwSecond;
 	}
 	if (remTime>1){
-		getVedioByTime(IUserID, inputChanel, &splitStartTime, &ci.endTime, ci.path);
+		++fn;
+		fc = videoFileCount(IUserID, inputChanel, &splitStartTime, &ci.endTime);
+		if (fc > 0){
+			if (ci.path == NULL){
+				getVedioByTime(IUserID, inputChanel, &splitStartTime, &splitEndTime, nullptr);
+			}
+			else{
+				string storePath = path + "_";
+				char fnChar[5];
+				sprintf_s(fnChar, "%d", fn);
+				storePath += fnChar;
+				storePath += ".mp4";
+				getVedioByTime(IUserID, inputChanel, &splitStartTime, &ci.endTime, const_cast<char*>(storePath.c_str()));
+				cout << "progress:100" << endl;
+			}
+		}
+		
+	}
+	if (fc <= 0){
+		
+		cout << "error:no video file exist!" << endl;
 	}
 	NET_DVR_Logout(IUserID);
 	NET_DVR_Cleanup();
